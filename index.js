8040a86c5600320f57504676a7693c458608f631
Sandra Felicio <mjpfelicia@gmail.com>
2023-09-24 09:01:07 -0300

    execultando exercico primeiro exercicio javascrip::

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const prompt = require("prompt-sync")();
 // Calcule a soma e a média
 
 // Mostre na tela o valor da soma e média
+
+
+
+
+const n1 = parseInt(prompt('Informe o primeiro número:'));
+const n2 = parseInt(prompt('Informe o segundo número:'));
+const n3 = parseInt(prompt('Informa o terceiro número:'));
+
+const media = (n1 + n2 + n3) / 3;
+
+
+ console.log("o primeiro número", n1)
+ console.log("o segundo número", n2)
+ console.log("o terceiro número", n3)
+ console.log("A media ficou em:", media)
+
+
+
