b5fe07712717003aa648ad6e2cb56cdd968c1401
Sandra Felicio <mjpfelicia@gmail.com>
2023-09-26 19:56:40 -0300

    correcao

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const prompt = require("prompt-sync")();
 const n1 = parseInt(prompt('Informe o primeiro número:'));
 const n2 = parseInt(prompt('Informe o segundo número:'));
 const n3 = parseInt(prompt('Informa o terceiro número:'));
-const soma =(n1 + n2 + n3);
+const soma = (n1 + n2 + n3);
 
 const media = (n1 + n2 + n3) / 3;
 
-console.log("A soma e",soma)
- console.log("o primeiro número", n1)
- console.log("o segundo número", n2)
- console.log("o terceiro número", n3)
- console.log("A media ficou em:", media)
+console.log("o primeiro número", n1)
+console.log("o segundo número", n2)
+console.log("o terceiro número", n3)
+console.log("A soma e", soma)
+console.log("A media ficou em:", media)
 
 
 
